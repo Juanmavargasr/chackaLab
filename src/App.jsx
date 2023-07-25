0b3dc4a7fd7f9b66381f26e50e7d6d970791a35f
Juan Manuel Vargas <juanmavargasr@gmail.com>
2023-07-25 09:05:21 -0500

    chakralab solved

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import Header from "./components/Header";
+import { theme } from "./resources/themes/index";
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Header></Header>
+    </ChakraProvider>
+  );
+}
+
+export default App;
